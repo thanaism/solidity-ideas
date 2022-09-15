49315a05db29b3b4e95c9e413477aea5e05f7e01
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-09-15 23:49:48 +0900

    hardhat-gas-reporterを導入

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,21 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'solidity-coverage';
+import 'hardhat-gas-reporter';
 
 // eslint-disable-next-line node/no-missing-require
 require('./scripts/tasks');
 
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
+  gasReporter: {
+    coinmarketcap: process.env.COIN_MARKET_CAP,
+    currency: 'JPY',
+    // token: 'MATIC',
+    // gasPriceApi: 'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',
+    token: 'ETH',
+    gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
+  },
 };
 
 export default config;
